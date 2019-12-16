4f35aa2df15368c81bad6b4e5c580f7b9e116d61
myungho <skqoaudgh2@hanmail.net>
2019-12-16 16:37:57 +0900

    main article을 API를 통해 동적으로 생성하도록

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     let key = 'AIzaSyAKol0ai1U9nSW4NEXHQ57vEz2sG926WNc';
-    let playlistId = 'PLVj73insf4VDeQEgraOpL7DNm4tHoSp63';
+    let playlistId = 'PLVj73insf4VCwtSsS0b6IJDO4uvtYA1Vc';
     let url = new URL('https://www.googleapis.com/youtube/v3/playlistItems');
 
     let option = {
@@ -20,6 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
         .then(data => {
             let id = data.items[0].snippet.resourceId.videoId;
             mainVideo(id);
+            resultsLoop(data.items);
         });
     }
 
@@ -39,5 +40,25 @@ document.addEventListener('DOMContentLoaded', function() {
                 allowfullscreen>
             </iframe>
         `;
+    }
+
+    function resultsLoop(data) {
+        const main = document.getElementsByTagName('main')[0];
+        main.innerHTML = '';
+
+        data.forEach(item => {
+            const thumb = item.snippet.thumbnails.medium.url;
+            const title = item.snippet.title;
+            const description = item.snippet.description;
+            main.innerHTML += `
+                <article>
+                    <img src=${thumb} alt="" class="thumb">
+                    <div class="details">
+                        <h4>${title}</h4>
+                        <p>${description}</p>
+                    </div>
+                </article>
+            `;
+        });
     }
 });
